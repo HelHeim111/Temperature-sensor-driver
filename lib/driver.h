2fab0e4bfbc6862542e50462eaaa6ef7dd42156f
HelHeim111 <dwp2004@icloud.com>
2024-03-30 23:09:54 +0100

    Added test cases, fixed multiple errors

--- lib/driver.h
+++ lib/driver.h
@@ -5,25 +5,28 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+static uint32_t device_register = 0;
+
+
 #define TSR_DATA_MASK       0xFF
-#define TSR_DATA(x)         (((uint32_t)x)|(device_register & (~TSR_DATA_MASK)))
+#define TSR_DATA(x)         ((uint32_t)(x < 0 ? (uint32_t)(x & TSR_DATA_MASK) : (uint32_t)(x)) | (device_register & (~TSR_DATA_MASK)))
 #define TSR_PWR_MASK        0x100
 #define TSR_PWR_SHIFT       8
 #define TSR_PWR(x)          (((uint32_t)x << TSR_PWR_SHIFT)|(device_register & (~TSR_PWR_MASK)))
-#define TSR_ERR_MASK        0x200
+#define TSR_ERR_MASK        0x600
 #define TSR_ERR_SHIFT       9
 #define TSR_ERR(x)          (((uint32_t)x << TSR_ERR_SHIFT)|(device_register & (~TSR_ERR_MASK)))
-#define TSR_RDF_MASK        0x400
-#define TSR_RDF_SHIFT       10
+#define TSR_RDF_MASK        0x800
+#define TSR_RDF_SHIFT       11
 #define TSR_RDF(x)          (((uint32_t)x << TSR_RDF_SHIFT)|(device_register & (~TSR_RDF_MASK)))
 
 void device_init();
 
-uint32_t get_temperature();
+int8_t get_temperature();
 
 uint32_t get_register();
 
-void write_mock_temperature(uint32_t val);
+void write_mock_temperature(int32_t val);
 
 bool is_device_powered_on();
 
--- src/driver.c
+++ src/driver.c
@@ -2,23 +2,22 @@
 
 /*
 * 31-16 reserved
-* 15-11 status flags, reserved for future use
-* 10 data ready flag (0 - not ready, 1 - ready)
-* 9 error (0 - ok, 1 - error)
+* 15-12 status flags, reserved for future use
+* 11 data ready flag (0 - not ready, 1 - ready)
+* 9-10 error (00 - ok, 01 - data overflow, 10 - data underflow, 11 - sensor malfunctioning)
 * 8 power status (0 - off, 1 - on)
 * 7-0 data
 */
-static uint32_t device_register = 0;
 
 void device_init() {
-    device_register = TSR_DATA(0);
-    device_register = TSR_ERR(0);
-    device_register = TSR_PWR(1);
-    device_register = TSR_RDF(1);
+    device_register = TSR_DATA(0b0);
+    device_register = TSR_ERR(0b00);
+    device_register = TSR_PWR(0b1);
+    device_register = TSR_RDF(0b1);
 }
 
-uint32_t get_temperature() {
-    uint32_t data = 0;
+int8_t get_temperature() {
+    int8_t data = 0;
     if(is_device_powered_on() && !is_error() && is_data_ready()) {
         if(device_register & (1 << 7)) {
             data = ((~device_register) + 0x1) & TSR_DATA_MASK;
@@ -27,7 +26,7 @@ uint32_t get_temperature() {
         else {
             data = device_register & TSR_DATA_MASK;
         }
-        device_register = TSR_RDF(0);
+        device_register = TSR_RDF(0b0);
     }
     return data;
 }
@@ -36,13 +35,23 @@ uint32_t get_register() {
     return device_register;
 }
 
-void write_mock_temperature(uint32_t val) {
-    if (val > 127 || val <= -128) {
-        device_register = TSR_ERR(1);
+void write_mock_temperature(int32_t val) {
+    if (val > 127) {
+        device_register = TSR_ERR(0b01);
         return;
     }
+    
+    if (val < -128) {
+        device_register = TSR_ERR(0b10);
+        return;
+    }
+    
+    if (((get_register() >> TSR_ERR_SHIFT) & 0b11) == 0b01 || ((get_register() >> TSR_ERR_SHIFT) & 0b11) == 0b10 ) {
+        device_register = TSR_ERR(0b00);
+    }
+    
     device_register = TSR_DATA(val);
-    device_register = TSR_RDF(1);
+    device_register = TSR_RDF(0b1);
 }
 
 bool is_device_powered_on() {
@@ -57,12 +66,16 @@ bool is_data_ready() {
     return 1 & (device_register >> TSR_RDF_SHIFT);
 }
 
-void main () {
-    device_register = 0xFFFF4411;
-    device_init();
-    if(is_device_powered_on()) {
-        printf("on\n");
-    }
+// void main () {
+//     device_register = 0xFFFF4411;
+//     device_init();
+//     write_mock_temperature(-800);
+//     if(!is_error()) {
+//         printf("no error\n");
+//     }
+    
+//     printf("%x\n", get_register());
 
-    return 0;
-}
+
+//     return 0;
+// }
